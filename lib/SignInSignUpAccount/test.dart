124ed320d418ff65701cd429559d4bc75398537c
mac <archana.pawar@tekzee.com>
2020-11-19 13:48:02 +0530

    upload in new repository

---
+++ lib/SignInSignUpAccount/test.dart
@@ -0,0 +1,23 @@
+/*
+Languge Selection
+Sign In
+Sign Up with barber and normal account
+barber list
+barber detail
+barber product
+Barber product Description
+Purchase product
+Check And Select Availabilty
+Make Appointment
+Online Payment (accountno 44444444444  pin 1234)
+Final Submission Payment Done
+Menu Design
+My Account
+Edit Billing Detail
+Edit Other Detail
+Preference
+Terms
+FAQS
+Logout
+
+ */import 'dart:html';
